adbfd98c883519c0ee4e9433d1992a33f335b552
eyakcn <eyakcn@gmail.com>
2014-09-17 23:01:49 +0900

    allow check questionnaire answers

--- src/main/java/io/sophone/Main.java
+++ src/main/java/io/sophone/Main.java
@@ -1,16 +1,71 @@
 package io.sophone;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.jetdrone.vertx.yoke.Middleware;
 import com.jetdrone.vertx.yoke.Yoke;
-import com.jetdrone.vertx.yoke.middleware.BodyParser;
-import com.jetdrone.vertx.yoke.middleware.ErrorHandler;
-import com.jetdrone.vertx.yoke.middleware.Limit;
-import com.jetdrone.vertx.yoke.middleware.Static;
+import com.jetdrone.vertx.yoke.middleware.*;
+import org.apache.commons.lang3.StringUtils;
+import org.vertx.java.core.json.JsonArray;
 import org.vertx.java.platform.Verticle;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.*;
 
 /**
  * Created by eyakcn on 2014/8/20.
  */
 public class Main extends Verticle {
+    private static final String delimiter = " | ";
+    private static final String baseDir = System.getProperty("user.home") + "/";
+    private static final String answerFilePath;
+    private static final List<Map<String, Integer>> statistic = new ArrayList<>();
+    private static final List<List<String>> parsedAnswers = new ArrayList<>();
+
+    static {
+        answerFilePath = baseDir + "questionnaire.txt";
+        File answerFile = new File(answerFilePath);
+        try {
+            if (answerFile.exists()) {
+                List<String> lines = Files.readAllLines(answerFile.toPath());
+                lines.forEach(line -> analyzeLine(line));
+            } else {
+                answerFile.createNewFile();
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void analyzeLine(String line) {
+        if (line == null || line.length() == 0) {
+            return;
+        }
+        String[] answers = StringUtils.splitByWholeSeparatorPreserveAllTokens(line, delimiter);
+        for (int i = 0; i < answers.length; i++) {
+            String answer = answers[i];
+
+            Map<String, Integer> answerMap = null;
+            if (statistic.size() == i) {
+                answerMap = new LinkedHashMap<>(6); // at most 6 choices
+                statistic.add(answerMap);
+            } else {
+                answerMap = statistic.get(i);
+            }
+
+            if (answerMap.containsKey(answer)) {
+                Integer count = answerMap.get(answer);
+                answerMap.put(answer, count++);
+            } else {
+                answerMap.put(answer, 1);
+            }
+        }
+        parsedAnswers.add(Arrays.asList(answers));
+    }
 
     @Override
     public void start() {
@@ -19,6 +74,60 @@ public class Main extends Verticle {
         app.use(new Limit(4096));
         app.use(new BodyParser());
         app.use(new Static("webroot", 0, true, false));
+        app.use(getRouter());
         app.listen(8181);
+    }
+
+    public Middleware getRouter() {
+        return new Router()
+                .get("/osaka/counting", request -> {
+                    GetResult returnVal = new GetResult();
+                    returnVal.statistic = statistic;
+                    returnVal.answers = parsedAnswers;
+                    Gson gson = new GsonBuilder().create();
+                    String content = gson.toJson(returnVal);
+                    container.logger().info("Get Result: " + content);
+
+                    request.response().putHeader("Content-Type", "application/json");
+                    try {
+                        request.response().putHeader("Content-Length", "" + content.getBytes("UTF-8").length);
+                    } catch (UnsupportedEncodingException e) {
+                        throw new RuntimeException(e);
+                    }
+                    request.response().write(content);
+                    request.response().end();
+                })
+                .post("/osaka/counting", request -> {
+                    List<String> answers = ((JsonArray) request.body()).toList();
+                    String line = StringUtils.join(answers, delimiter);
+                    analyzeLine(line);
+
+                    List<String> lines = new ArrayList<String>();
+                    lines.add(line);
+                    File answerFile = new File(answerFilePath);
+                    try {
+                        Files.write(answerFile.toPath(), lines, StandardOpenOption.APPEND);
+                        container.logger().info("New answer add to: " + answerFilePath);
+                    } catch (IOException e) {
+                        container.logger().error("Failed to write answers file!" + e.toString());
+                    }
+
+//                    vertx.fileSystem().writeFile(answerFilePath, new Buffer(line).appendString("\n"), result -> {
+//                        if (!result.succeeded()) {
+//                            container.logger().error("Failed to write answers file!" + result.cause());
+//                        } else {
+//                            container.logger().info("New answer add to: " + answerFilePath);
+//                        }
+//                    });
+
+                    String ipAddress = request.ip();
+                    container.logger().info("Submit from: " + ipAddress);
+                    request.response().end();
+                });
+    }
+
+    private static class GetResult {
+        public List<Map<String, Integer>> statistic;
+        public List<List<String>> answers;
     }
 }
--- src/main/resources/webroot/osaka/js/main.js
+++ src/main/resources/webroot/osaka/js/main.js
@@ -7,16 +7,15 @@ $(function() {
     var $question = $choice.parents('.question:first');
     var $answer = $question.find('.answer:first');
 
-    var choice = $choice.data('key');
-    $answer.text(choice);
-    $answer.removeClass('empty');
+    $answer.val($choice.data('key'));
+    $answer.trigger('input');
     $answer.data('full', $choice.text());
   });
 
-  $('body').on('input.textarea', 'textarea.answer', function(e) {
-    var $textarea = $(e.target);
-    var empty = !$.trim($textarea.val()).length;
-    $textarea.toggleClass('empty', empty);
+  $('body').on('input.answer', '.answer', function(e) {
+    var $input = $(e.target);
+    var empty = $.trim($input.val()).length == 0;
+    $input.toggleClass('empty', empty);
   });
 
   $('body').on('click.img-link', 'div.img-link', function(e) {
@@ -31,5 +30,76 @@ $(function() {
   $('body').on('click.other', function() {
     var $preview = $('#img-preview');
     $preview.hide();
+  });
+
+  $('#submit-btn').click(function() {
+    $(this).hide();
+    var result = [];
+    $('.question').each(function() {
+      var $question = $(this);
+      var $answer = $(this).find('.answer:first');
+
+      var questionNumber = $question.data('number');
+      var answerText = $answer.val();
+
+      result.push(answerText);
+    });
+    $.ajax({
+      url: '/osaka/counting',
+      type: "POST",
+      data: JSON.stringify(result),
+      contentType: "application/json; charset=utf-8",
+      dataType: "json",
+      success: function() {
+        confirm("提出しました！どうもありがとうございます。");
+      }
+    });
+  });
+
+  var admin = false;
+  $('#admin').click(function() {
+    if (admin) {
+      return;
+    }
+    var code = prompt("", "");
+    if (code == 'woaiguangjian') {
+      admin = true;
+      switchToAdmin();
+    }
+  });
+
+  function switchToAdmin() {
+    $('.choice').removeClass('choice');
+    $('textarea').prop('readonly', true);
+    $('[non-admin]').hide();
+    $('.answer').val('');
+    $.get('/osaka/counting', function(data) {
+      statistic = data['statistic'];
+      answersAry = data['answers'];
+      if (answersAry && answersAry.length > 0) {
+        $('#answer-switch').attr('max', answersAry.length);
+        $('#admin').find('div:first').show();
+      }
+    });
+  }
+
+  $('#statistic-btn').click(function() {
+    $('.answer').val('');
+    // TODO statistics
+  });
+
+  $('#answer-switch').change(function() {
+    var idx = $(this).val() - 1;
+    var answers = answersAry[idx];
+    $('.question').each(function() {
+      var $question = $(this);
+      var $answer = $(this).find('.answer:first');
+
+      var questionNumber = $question.data('number');
+      var answerText = answers[questionNumber - 1];
+
+      $answer.val(answerText);
+      $answer.trigger('input');
+    });
   });
 });
