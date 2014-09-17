953d59f9e24b2df0bb056b4ee06803125e10def8
eyakcn <eyakcn@gmail.com>
2014-09-18 07:55:28 +0900

    support statistics

--- src/main/resources/webroot/osaka/js/main.js
+++ src/main/resources/webroot/osaka/js/main.js
@@ -3,6 +3,9 @@ $(function() {
   $preview.hide();
 
   $('body').on('click.choice', 'div.choice', function(e) {
+    if (admin) {
+      return;
+    }
     var $choice = $(e.target);
     var $question = $choice.parents('.question:first');
     var $answer = $question.find('.answer:first');
@@ -69,10 +72,10 @@ $(function() {
   });
 
   function switchToAdmin() {
-    $('.choice').removeClass('choice');
     $('textarea').prop('readonly', true);
     $('[non-admin]').hide();
     $('.answer').val('');
+    $('.answer').trigger('input');
     $.get('/osaka/counting', function(data) {
       statistic = data['statistic'];
       answersAry = data['answers'];
@@ -85,7 +88,61 @@ $(function() {
 
   $('#statistic-btn').click(function() {
     $('.answer').val('');
-    // TODO statistics
+    $('.answer').trigger('input');
+
+    $('.question').each(function() {
+      var $question = $(this);
+      var $answer = $(this).find('.answer:first');
+      $answer.hide();
+
+      var questionNumber = $question.data('number');
+      var answerMap = statistic[questionNumber - 1];
+
+      if ($question.find('.choice').length > 0) {
+        $question.find('.choice').each(function() {
+          var $choice = $(this);
+          var key = $choice.data('key');
+          var count = answerMap[key] || 0;
+
+          var $count = $choice.find('> .count');
+          if ($count.length == 0) {
+            $count = $('<div class="count"></div>');
+            $choice.append($count);
+          }
+          $count.text(count);
+          $count.toggleClass('empty', count == 0);
+        });
+      } else {
+        var newCnt = 0;
+        $.each(answerMap, function(choice, count) {
+          if (choice == '') {
+            return true; // continue;
+          }
+          newCnt++;
+          var $choice = $('<div class="choice col-md-2"></div>');
+          $choice.text(newCnt + '. ' + choice);
+          $count = $('<div class="count"></div>');
+          $choice.append($count);
+          $count.text(count);
+          $count.toggleClass('empty', count == 0);
+
+          $choice.insertBefore($answer);
+        });
+      }
+
+      var total = 0;
+      $.each(answerMap, function(choice, count){
+        total += count;
+      });
+      var emptyCount = answerMap[''] || 0;
+      var $count = $question.find('> .count');
+      if ($count.length == 0) {
+        $count = $('<div class="count"></div>');
+        $question.append($count);
+      }
+      $count.text(total + ' - ' + emptyCount + ' = ' + (total - emptyCount));
+      $count.toggleClass('empty', total == 0);
+    });
   });
 
   $('#answer-switch').change(function() {
@@ -94,6 +151,7 @@ $(function() {
     $('.question').each(function() {
       var $question = $(this);
       var $answer = $(this).find('.answer:first');
+      $answer.show();
 
       var questionNumber = $question.data('number');
       var answerText = answers[questionNumber - 1];
@@ -101,5 +159,34 @@ $(function() {
       $answer.val(answerText);
       $answer.trigger('input');
     });
+  });
+
+  $('body').keydown(function(e) {
+    if (!admin) {
+      return;
+    }
+    var $switch = $('#answer-switch')
+    var minVal = $switch.attr('min');
+    var maxVal = $switch.attr('max');
+    var current = $switch.val();
+    if (current == '') {
+      current = 0;
+    }
+    switch (e.keyCode) {
+    case 37: // left
+      if (current > minVal) {
+        current--;
+        $switch.val(current);
+        $switch.trigger('change');
+      }
+      break;
+    case 39: // right
+      if (current < maxVal) {
+        current++;
+        $switch.val(current);
+        $switch.trigger('change');
+      }
+      break;
+    }
   });
 });
