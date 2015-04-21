63fd47c9be5ca63813016ebe2da81feec02815a0
SONG Peng <eyakcn@gmail.com>
2015-04-21 12:06:19 +0800

    new feild for unionid

--- src/main/java/io/sophone/wechat/OAuth2Token.java
+++ src/main/java/io/sophone/wechat/OAuth2Token.java
@@ -9,6 +9,7 @@ public class OAuth2Token {
     public String refresh_token; // 用户刷新access_token
     public String openid; // 用户唯一标识，请注意，在未关注公众号时，用户访问公众号的网页，也会产生一个用户和公众号唯一的OpenID
     public String scope; // 用户授权的作用域，使用逗号（,）分隔
+    public String unionid; // 只有在用户将公众号绑定到微信开放平台账号后，才会出现该字段
 
     public String errcode;
     public String errmsg;
--- src/main/java/io/sophone/wechat/SnsUser.java
+++ src/main/java/io/sophone/wechat/SnsUser.java
@@ -16,6 +16,7 @@ public final class SnsUser {
     public String language; // 语言
     public String headimgurl; //用户头像，最后一个数值代表正方形头像大小（有0、46、64、96、132数值可选，0代表640*640正方形头像），用户没有头像时该项为空
     public List<String> privilege; // 用户特权信息，json 数组，如微信沃卡用户为（chinaunicom）
+    public String unionid;
 
     public String errcode;
     public String errmsg;
